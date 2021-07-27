8e7d4c36d262c2ac9a2368312c8578bdd3f7e70e
Alexander Granovski <alexxgranovski@gmail.com>
2021-07-28 00:24:50 +0300

    feat: lesson-4 tasks implementation

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const container = document.createElement('ul');
+  friends.forEach((friend) => {
+    const element = document.createElement('li');
+    const content = `${friend.firstName}${friend.lastName}`;
+    element.textContent = content;
+    container.appendChild(element);
+  });
+  return container;
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let countIndex = 0;
+  const collTr = table.rows;
+  for (let t = 0; t < collTr.length; t += 1) {
+    const collTd = collTr[t].cells;
+    for (let i = 0; i < collTd.length; i += 1) {
+      if (countIndex === i) {
+        collTd[i].style.backgroundColor = 'red';
+      }
+    }
+    countIndex += 1;
+  }
 }
