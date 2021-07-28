bee05dcf25c082ab3f8d283c74fe86e45308cf93
Alexander Granovski <alexxgranovski@gmail.com>
2021-07-28 22:18:55 +0300

    refactor: Improved the algorithm, without changing the functionality

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,10 +3,8 @@ function makeDiagonalRed(table) {
   const collTr = table.rows;
   for (let t = 0; t < collTr.length; t += 1) {
     const collTd = collTr[t].cells;
-    for (let i = 0; i < collTd.length; i += 1) {
-      if (countIndex === i) {
-        collTd[i].style.backgroundColor = 'red';
-      }
+    if (countIndex === t) {
+      collTd[t].style.backgroundColor = 'red';
     }
     countIndex += 1;
   }
