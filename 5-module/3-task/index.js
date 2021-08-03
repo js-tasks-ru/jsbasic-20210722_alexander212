3a45e2f5cf807b193956ef8ffa88ac33d86ecbf5
Alexander Granovski <alexxgranovski@gmail.com>
2021-08-04 00:25:11 +0300

    feat: lesson-5 tasks implementation

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+  const containerSlides = document.querySelector('.carousel__inner');
+  const slides = document.querySelectorAll('.carousel__slide');
+  const btnLeft = document.querySelector('.carousel__arrow_left');
+  const btnRight = document.querySelector('.carousel__arrow_right');
+  btnLeft.style.display = 'none';
+
+  const slideWidth = slides[0].offsetWidth;
+  let currentSlideCount = 1;
+  let currentOffset = 0;
+
+  carousel.addEventListener('click', (e) => {
+    if (e.target.closest('.carousel__arrow_right')) {
+      currentSlideCount += 1;
+      currentOffset += slideWidth;
+      containerSlides.style.transform = `translateX(-${currentOffset}px)`;
+    } else if (e.target.closest('.carousel__arrow_left')) {
+      currentOffset = currentOffset - slideWidth;
+      containerSlides.style.transform = `translateX(-${currentOffset}px)`;
+      currentSlideCount -= 1;
+    }
+    if (currentSlideCount > 1) {
+      btnLeft.style.display = 'flex';
+    } else {
+      btnLeft.style.display = 'none';
+    } if (currentSlideCount === slides.length) {
+      btnRight.style.display = 'none';
+    } else {
+      btnRight.style.display = 'flex';
+    }
+  });
 }
