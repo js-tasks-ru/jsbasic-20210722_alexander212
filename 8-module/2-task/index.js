9de5f279d6919db6a95bcc6c694756beddd4ed1a
Alexander Granovski <alexxgranovski@gmail.com>
2021-08-30 01:16:39 +0300

    feat: lesson-7 task 'cart-1' implementation

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,59 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(`<div class="products-grid">
+    <div class="products-grid__inner">
+    </div>
+    </div>`);
+    this.elem.querySelector('.products-grid__inner').append(this.createProducts(this.products));
+  }
+
+  createProducts(products) {
+    const container = document.createDocumentFragment();
+    products.forEach((product) => {
+      const newProduct = new ProductCard(product);
+      container.append(newProduct.elem);
+    });
+    return container;
+  }
+
+  updateFilter(filters) {
+    const container = this.elem.querySelector('.products-grid__inner');
+    this.filters = {...this.filters, ...filters};
+    const keys = Object.keys(this.filters);
+    let result = this.products.filter((p) => {
+      for (let key of keys) {
+        switch (key) {
+        case 'noNuts':
+          if (!p['nuts']) {
+            break;
+          }
+        case 'vegeterianOnly':
+          if (p['vegeterian']) {
+            break;
+          }
+        case 'maxSpiciness':
+          if (p['spiciness'] <= this.filters[key]) {
+            break;
+          }
+        case 'category':
+          const categoryFilter = this.filters[key];
+          if (!categoryFilter) {
+            break;
+          } else if (p['category'] === this.filters['category']) {
+            break;
+          }
+        default:
+          return false;
+        }
+      }
+      return true;
+    });
+    container.innerHTML = '';
+    container.append(this.createProducts(result));
   }
 }
--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,55 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return;
+    }
+    let isContain = false;
+    if (this.cartItems.length >= 1) {
+      this.cartItems.forEach((i) => {
+        if (i.product.id === product.id) {
+          i.count += 1;
+          isContain = true;
+        }
+      });
+    }
+    if (!isContain) {
+      this.cartItems.push({
+        product,
+        count: 1,
+      });
+    }
+    console.log(this.cartItems)
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    this.cartItems.forEach((item, index) => {
+      if (item.product.id === productId) {
+        const count = item.count + amount;
+        if (count <= 0) {
+          this.cartItems = [...this.cartItems.slice(0, index), ...this.cartItems.slice(index + 1,)];
+        } else {
+          item.count = count;
+        }
+      }
+    });
+    console.log(this.cartItems)
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length > 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((acc, item) => {
+      return acc + item.count;
+    }, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((acc, item) => {
+      return acc + item.product.price * item.count;
+    }, 0);
   }
 
   onProductUpdate(cartItem) {
